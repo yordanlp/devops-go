edc47ef3b3cb854631757a7691c2838f41a9b576
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-05-24 19:22:31 +0200

    adding code

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Simple struct {
+	Name        string
+	Description string
+	Url         string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	simple := Simple{"Hello", "World", r.Host}
+
+	jsonOutput, _ := json.Marshal(simple)
+
+	fmt.Fprintln(w, string(jsonOutput))
+}
+
+func main() {
+	fmt.Println("Server started on port 4444")
+	http.HandleFunc("/api", handler)
+	log.Fatal(http.ListenAndServe(":4444", nil))
+}
